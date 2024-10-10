c18c496821652ed0b6e74381b8c3b04665f9619b
Jan-Ole Koslik <101901166+janoleko@users.noreply.github.com>
2024-10-10 10:36:46 +0200

    adding simulation of multiple data sets for CIs of decoding accuracy

--- simulation_study/Simulation_study_part3.R
+++ simulation_study/Simulation_study_part3.R
@@ -10,6 +10,7 @@ library(LaMa)
 # Part 2: aggregate sizes -------------------------------------------------
 
 # parameter for sim model
+N = 3
 beta = matrix(c(log(c(8,7,6)), -0.2, 0.2, -0.6, 0.3, -0.2, -0.4), nrow = 3)
 omega = matrix(c(0, 0.7, 0.3, 
                  0.2, 0, 0.8,
@@ -43,7 +44,8 @@ sigma = stateparams$sigma
 kappa = stateparams$kappa
 delta = c(sum(data$C==1), sum(data$C==2), sum(data$C==3))/nrow(data)
 
-pdf("./figures/simulation/state_dependent.pdf", width=8, height = 4)
+# pdf("./figures/simulation/state_dependent.pdf", width=8, height = 4)
+color = c("orange", "deepskyblue", "seagreen2")
 par(mfrow = c(1,2), xpd = F)
 hist(data$step, prob = T, breaks = 100, bor = "white", main = "", 
      ylim = c(0, 0.0025), xlim = c(0,2500), ylab = "density", xlab = "turning angle")
@@ -60,7 +62,8 @@ curve(delta[3]*CircStats::dvm(x, 0, kappa[3]), add=T, lwd=2, col = color[3], n =
 curve(delta[1]*CircStats::dvm(x, 0, kappa[1])+
         delta[2]*CircStats::dvm(x, 0, kappa[2])+
         delta[3]*CircStats::dvm(x, 0, kappa[3]), add=T, lwd=2, lty=2, n=500)
-dev.off()
+# dev.off()
+
 
 
 # Model fitting -----------------------------------------------------------
@@ -81,7 +84,7 @@ thetainit = c(log(c(stateparams$mu,
               rep(0,N*(N-2))
 )
 
-# cl = makeCluster(7); setDefaultCluster(cl=cl)
+# cl = makeCluster(8); setDefaultCluster(cl=cl)
 # modpHSMM_optim = optimParallel(par=thetainit, fn=mllkpHSMM, X=data, Z=Z, ind=ind, agsizes=agsizes,
 #                          parallel=list(forward=TRUE),
 #                          control = list(trace=4, maxit=1e4, ndeps=rep(1e-5,length(thetainit))))
@@ -114,6 +117,7 @@ thetainit = c(log(c(stateparams$mu,
 # saveRDS(modpHMM, "./simulation_study/models/modpHMM.rds")
 
 modpHMM = readRDS("./simulation_study/models/modpHMM.rds")
+
 
 ## Model 3: homogeneous HSMM
 
@@ -377,6 +381,7 @@ for(mod in 1:3){
 
 dev.off()
 
+
 # Pseudo resiudals --------------------------------------------------------
 
 # functions to calculate cdf transform
@@ -448,4 +453,269 @@ for(m in 1:3){
 }
 
 
+
+# Decoding accuracies with significance -----------------------------------
+
+## fit each model 100 time for accuracy CI
+library(foreach)
+library(doParallel)
+
+B = 500
+
+# simulate 100 data sets
+set.seed(123)
+Data = vector("list", B)
+for(i in 1:B){
+  print(i)
+  Data[[i]] = sim_phsmm(1e4, beta, omega, stateparams)
+} 
+
+
+## Model 1: true periodic HSMM
+L = 24; K = 1; N = 3
+Z = LaMa::trigBasisExp(1:L-1, L=L, degree=K)
+# indexshift is because in LaMa gamma^(t) = Pr(S_t | S_t-1) while in this paper gamma^(t) = Pr(S_t+1 | S_t)
+
+thetainit = c(log(c(stateparams$mu,
+                    stateparams$sigma,
+                    stateparams$kappa)),
+              as.numeric(beta),
+              rep(0,N*(N-2))
+)
+
+cl = makeCluster(parallel::detectCores()-1)
+registerDoParallel(cl)
+clusterExport(cl, c("Data", "Z", "agsizes", "nlm", "mllkpHSMM"))
+acc_phsmm = foreach(i = 1:B, .errorhandling = "pass") %dopar% {
+  data = Data[[i]]
+  ind = which(!is.na(data$step) & !is.na(data$angle))
+  thismod = nlm(mllkpHSMM, thetainit, X=data, Z=Z, ind=ind, agsizes=agsizes,
+                iterlim = 1e3, print.level = 0, stepmax = 10)
+  theta.star = thismod$estimate
+  X = data; n = nrow(X)
+  mu = exp(theta.star[1:N])
+  sigma = exp(theta.star[N+1:N])
+  kappa = exp(theta.star[2*N + 1:N])
+  beta = matrix(theta.star[3*N + 1:(N*(1+2*K))], nrow = N, ncol = 1+2*K)
+  Lambda = exp(cbind(1,Z)%*%t(beta))
+  if(N>2){ # only needed if N>2
+    omega = matrix(0,N,N)
+    omega[!diag(N)] = as.vector(t(matrix(c(rep(1,N),exp(theta.star[3*N+N*(1+2*K)+1:(N*(N-2))])),N,N-1)))
+    omega = t(omega)/apply(omega,2,sum)
+  }else{ omega = matrix(c(0,1,1,0),2,2) }
+  dm = list()
+  for(j in 1:N) dm[[j]] = sapply(1:agsizes[j]-1, dpois, lambda = Lambda[,j])
+  Gamma = LaMa::tpm_phsmm(omega, dm)
+  delta = LaMa::stationary_p(Gamma, t = X$tod[1])
+  allprobs = matrix(NA, nrow = n, ncol = N)
+  for(j in 1:N){
+    allprobs[ind,j] = dgamma(X$step[ind], shape=mu[j]^2/sigma[j]^2, scale=sigma[j]^2/mu[j])*
+      CircStats::dvm(X$angle[ind], mu = 0, kappa = kappa[j])
+  }
+  allprobs_large = t(apply(allprobs, 1, rep, times = agsizes))
+  rawstates_vit = LaMa::viterbi_p(delta, Gamma, allprobs_large, X$tod)
+  probs = LaMa::stateprobs_p(delta, Gamma, allprobs_large, X$tod)
+  rawstates_loc = apply(probs, 1, which.max)
+  if(is.list(rawstates_loc)){
+    rawstates_loc[which(sapply(rawstates_loc, length) == 0)] = NA
+    rawstates_loc = unlist(rawstates_loc)
+  }
+  states_vit = states_loc = rep(NA, length(rawstates_vit))
+  for(j in 1:N){
+    states_vit[which(rawstates_vit %in% c(c(0,cumsum(agsizes)[-N])[j]+1:agsizes[j]))] = j
+    states_loc[which(rawstates_loc %in% c(c(0,cumsum(agsizes)[-N])[j]+1:agsizes[j]))] = j
+  }
+  acc_vit = sum(states_vit == data$C) / length(states_vit)
+  acc_loc = sum(states_loc == data$C, na.rm = TRUE) / length(states_loc)
+
+  list(acc_vit=acc_vit, acc_loc=acc_loc)
+}
+stopCluster(cl)
+saveRDS(acc_phsmm, "./simulation_study/simulation_results/accuracy/acc_phsmm500.rds")
+
+
+## Model 2: periodic HMM
+
+Z = LaMa::trigBasisExp(1:L-1, L=L, degree=K)
+thetainit = c(log(c(stateparams$mu,
+                    stateparams$sigma,
+                    stateparams$kappa)),
+              rep(-2,N*(N-1)), rep(0, 2*N*(N-1)*K))
+
+cl = makeCluster(parallel::detectCores()-1)
+registerDoParallel(cl)
+clusterExport(cl, c("Data", "Z", "nlm", "mllkHMM"))
+acc_phmm = foreach(i = 1:B, .errorhandling = "pass") %dopar% {
+  data = Data[[i]]
+  ind = which(!is.na(data$step) & !is.na(data$angle))
+  thismod = nlm(mllkHMM, thetainit, X=data, Z=Z, ind=ind, K=1,
+                iterlim = 1e3, print.level = 0, stepmax = 50)
+  theta.star = thismod$estimate
+  X = data; n = nrow(X)
+  mu = exp(theta.star[1:N])
+  sigma = exp(theta.star[N+1:N])
+  kappa = exp(theta.star[2*N + 1:N])
+  beta = matrix(theta.star[3*N + 1:(N*(N-1)*(1+2*K))], nrow = N*(N-1), ncol = 1+2*K)
+  Gamma = LaMa::tpm_p(tod=1:L, L=L, beta=beta, degree=K, Z=Z)
+  delta = LaMa::stationary_p(Gamma, t = X$tod[1])
+  allprobs = matrix(NA, nrow = n, ncol = N)
+  for(j in 1:N){
+    allprobs[ind,j] = dgamma(X$step[ind], shape=mu[j]^2/sigma[j]^2, scale=sigma[j]^2/mu[j])*
+      CircStats::dvm(X$angle[ind], mu = 0, kappa = kappa[j])
+  }
+  states_vit = LaMa::viterbi_p(delta, Gamma, allprobs, X$tod)
+  probs = LaMa::stateprobs_p(delta, Gamma, allprobs, X$tod)
+  states_loc = apply(probs, 1, which.max)
+  if(is.list(states_loc)){
+    states_loc[which(sapply(states_loc, length) == 0)] = NA
+    states_loc = unlist(states_loc)
+  }
+  acc_vit = sum(states_vit == data$C) / length(states_vit)
+  acc_loc = sum(states_loc == data$C, na.rm = TRUE) / length(states_loc)
+
+  list(acc_vit=acc_vit, acc_loc=acc_loc)
+}
+stopCluster(cl)
+saveRDS(acc_phmm, "./simulation_study/simulation_results/accuracy/acc_phmm500.rds")
+
+
+## Model 3: homogeneous HSMM
+
+lambda = c(8,7,6) # exponentiated intercepts as starting value
+thetainit = c(log(c(stateparams$mu,
+                    stateparams$sigma,
+                    stateparams$kappa,
+                    lambda)),
+              rep(0,N*(N-2))
+)
+
+cl = makeCluster(parallel::detectCores()-1)
+registerDoParallel(cl)
+clusterExport(cl, c("Data", "Z", "nlm", "mllkHMM"))
+acc_hsmm = foreach(i = 1:B, .errorhandling = "pass") %dopar% {
+  data = Data[[i]]
+  ind = which(!is.na(data$step) & !is.na(data$angle))
+  thismod = nlm(mllkHSMM, thetainit, X=data, ind=ind, agsizes=agsizes,
+                iterlim = 1e3, print.level = 0, stepmax = 2)
+  theta.star = thismod$estimate
+  X = data; n = nrow(X)
+  mu = exp(theta.star[1:N])
+  sigma = exp(theta.star[N+1:N])
+  kappa = exp(theta.star[2*N + 1:N])
+  lambda = exp(theta.star[3*N + 1:N])
+  if(N>2){ # only needed if N>2
+    omega = matrix(0,N,N)
+    omega[!diag(N)] = as.vector(t(matrix(c(rep(1,N),exp(theta.star[4*N+1:(N*(N-2))])),N,N-1)))
+    omega = t(omega)/apply(omega,2,sum)
+  }else{ omega = matrix(c(0,1,1,0),2,2) }
+  dm = list()
+  for(j in 1:N){ dm[[j]] = dpois(1:agsizes[j]-1, lambda[j]) }
+  Gamma = LaMa::tpm_hsmm(omega, dm)
+  delta = LaMa::stationary(Gamma)
+  allprobs = matrix(NA, nrow = n, ncol = N)
+  for(j in 1:N){
+    allprobs[ind,j] = dgamma(X$step[ind], shape=mu[j]^2/sigma[j]^2, scale=sigma[j]^2/mu[j])*
+      CircStats::dvm(X$angle[ind], mu = 0, kappa = kappa[j])
+  }
+  allprobs_large = t(apply(allprobs, 1, rep, times = agsizes))
+  rawstates_vit = LaMa::viterbi(delta, Gamma, allprobs_large)
+  probs = LaMa::stateprobs(delta, Gamma, allprobs_large)
+  rawstates_loc = apply(probs, 1, which.max)
+  if(is.list(rawstates_loc)){
+    rawstates_loc[which(sapply(rawstates_loc, length) == 0)] = NA
+    rawstates_loc = unlist(rawstates_loc)
+  }
+  states_vit = states_loc = rep(NA, length(rawstates_vit))
+  for(j in 1:N){
+    states_vit[which(rawstates_vit %in% c(c(0,cumsum(agsizes)[-N])[j]+1:agsizes[j]))] = j
+    states_loc[which(rawstates_loc %in% c(c(0,cumsum(agsizes)[-N])[j]+1:agsizes[j]))] = j
+  }
+  acc_vit = sum(states_vit == data$C) / length(states_vit)
+  acc_loc = sum(states_loc == data$C, na.rm = TRUE) / length(states_loc)
+
+  list(acc_vit=acc_vit, acc_loc=acc_loc)
+}
+stopCluster(cl)
+saveRDS(acc_hsmm, "./simulation_study/simulation_results/accuracy/acc_hsmm500.rds")
+
+
+acc_phsmm = readRDS("./simulation_study/simulation_results/accuracy/acc_phsmm500.rds")
+acc_phmm = readRDS("./simulation_study/simulation_results/accuracy/acc_phmm500.rds")
+acc_hsmm = readRDS("./simulation_study/simulation_results/accuracy/acc_hsmm500.rds")
+
+
+phsmm_acc = phmm_acc = hsmm_acc = data.frame(vit = numeric(B), loc = numeric(B))
+for(i in 1:B){
+  phsmm_acc$vit[i] = acc_phsmm[[i]]$acc_vit
+  phsmm_acc$loc[i] = acc_phsmm[[i]]$acc_loc
+  
+  phmm_acc$vit[i] = acc_phmm[[i]]$acc_vit
+  phmm_acc$loc[i] = acc_phmm[[i]]$acc_loc
+  
+  hsmm_acc$vit[i] = acc_hsmm[[i]]$acc_vit
+  hsmm_acc$loc[i] = acc_hsmm[[i]]$acc_loc
+}
+
+phsmmCI_vit = quantile(phsmm_acc$vit, c(0.025, 0.975))
+phmmCI_vit = quantile(phmm_acc$vit, c(0.025, 0.975))
+hsmmCI_vit = quantile(hsmm_acc$vit, c(0.025, 0.975))
+
+
+adjust = 1.5 # KDE bandwith adjustment
+
+# pdf("./figures/simulation/accuracy.pdf", width = 7, height = 3.6)
+
+m = matrix(c(1,1,2,3), nrow = 2, ncol = 2, byrow = TRUE)
+layout(mat = m, heights = c(0.2, 1))
+par(mar = c(0.5,2,1,1))
+plot(1, type = "n", axes=FALSE, xlab="", ylab="", xlim = c(0,10), ylim = c(0,1))
+
+legend(x = 0.6, y = 1, lwd = 2, text.width = c(2, 1.8, 0),
+       col = color,
+       legend = c("periodic HSMM", "periodic HMM", "homogeneous HSMM"), bty = "n", horiz = TRUE)
+
+par(mar = c(5,4,4,2) + 0.1)
+plot(NA, xlim = c(0.94, 0.98), bty = "n", ylim = c(0, 150), 
+     main = "Viterbi decoding", xlab = "accuracy", ylab = "density")
+lines(density(phsmm_acc$vit, adjust = adjust), col = color[1], lwd = 2)
+lines(density(phmm_acc$vit, adjust = adjust), col = color[2], lwd = 2)
+lines(density(hsmm_acc$vit, adjust = adjust), col = color[3], lwd = 2)
+
+plot(NA, xlim = c(0.94, 0.98), bty = "n", ylim = c(0, 150), 
+     main = "Local decoding", xlab = "accuracy", ylab = "density")
+
+lines(density(phsmm_acc$loc, adjust = adjust), col = color[1], lwd = 2)
+lines(density(phmm_acc$loc, adjust = adjust), col = color[2], lwd = 2)
+lines(density(hsmm_acc$loc, adjust = adjust), col = color[3], lwd = 2)
+
+# dev.off()
+
+## averages
+# viterbi
+round(mean(phsmm_acc$vit)*100, 2)
+round(mean(phmm_acc$vit)*100, 2)
+round(mean(hsmm_acc$vit)*100, 2)
+# local
+round(mean(phsmm_acc$loc)*100, 2)
+round(mean(phmm_acc$loc)*100, 2)
+round(mean(hsmm_acc$loc)*100, 2)
+
+## confidence intervals
+# viterbi
+round(quantile(phsmm_acc$vit, c(0.025, 0.975))*100, 2)
+round(quantile(phmm_acc$vit, c(0.025, 0.975))*100, 2)
+round(quantile(hsmm_acc$vit, c(0.025, 0.975))*100, 2)
+# local
+round(quantile(phsmm_acc$loc, c(0.025, 0.975))*100, 2)
+round(quantile(phmm_acc$loc, c(0.025, 0.975))*100, 2)
+round(quantile(hsmm_acc$loc, c(0.025, 0.975))*100, 2)
+
+
+## paired t-test
+# viterbi
+t.test(phsmm_acc$vit, phmm_acc$vit, paired = TRUE)
+t.test(phsmm_acc$vit, hsmm_acc$vit, paired = TRUE)
+# local
+t.test(phsmm_acc$loc, phmm_acc$loc, paired = TRUE)
+t.test(phsmm_acc$loc, hsmm_acc$loc, paired = TRUE)
 
